958ff80d311c4a5db837beecdf1b853a3baab9de
bianca150100 <45423357+bianca150100@users.noreply.github.com>
2021-01-13 14:46:01 +0200

    last update

--- proiectDjangoOpt/statiune/models.py
+++ proiectDjangoOpt/statiune/models.py
@@ -5,24 +5,10 @@ from django.db import models
 #DataFlair Models
 
 class Camera(models.Model):
-    ROOM_CATEGORIES = (('da', 'cu baie'), ('nu', 'fara baie'))
+    categ = (('da', 'cu baie'), ('nu', 'fara baie'))
     numar_camera = models.IntegerField(default=0)
-    tip_camera = models.CharField(max_length=5, choices=ROOM_CATEGORIES)
+    tip_camera = models.CharField(max_length=5, choices=categ)
     nr_persoane = models.IntegerField(default=0)
-
-    def __str__(self):
-        return 'Room No. {} with {} and {} number of persons.'.format(self.numar_camera, self.tip_camera, self.nr_persoane)
-
-#nu mai e folosit!
-class Pensiune(models.Model):
-    nume = models.CharField(max_length = 50)
-    strada = models.CharField(max_length = 30, default='Str.Invalid')
-    adresaMail = models.EmailField(blank = True)
-    telefon = models.CharField(max_length = 10, default='07XXXXXXXX')
-    descriere = models.CharField(max_length = 600, default='No description')
-    camereRezervate = models.ManyToManyField(Camera)
-    def __str__(self):
-        return self.nume
 
 class RezervareCamere(models.Model):
     nume = models.CharField(max_length = 50);
